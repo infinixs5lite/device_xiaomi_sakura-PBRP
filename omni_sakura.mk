d92cfabb60e45dc28112b2c2ee51fdc23bb10088
infinixs5lite <kelvinchinedu97@gmail.com>
2024-01-26 08:52:54 +0100

    Update omni_sakura.mk

--- omni_sakura.mk
+++ omni_sakura.mk
@@ -25,6 +25,9 @@ $(call inherit-product, build/target/product/embedded.mk)
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/pb/config/common.mk)
 
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
+
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := sakura
 PRODUCT_NAME := omni_sakura
