bcdb7e74f0ce6ee515e183f7058fa58beffc3ce6
infinixs5lite <kelvinchinedu97@gmail.com>
2024-01-26 09:59:30 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -86,6 +86,21 @@ PLATFORM_SECURITY_PATCH := 2099-12-31
 VENDOR_SECURITY_PATCH := 2099-12-31
 PLATFORM_VERSION := 16.1.0
 
+BOARD_ROOT_EXTRA_FOLDERS := bt_firmware dsp firmware persist
+
+# Extra libs needed for crypto
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libicuuc \
+    libxml2 \
+    libion \
+    android.hidl.base@1.0
+
+TW_RECOVERY_ADDITIONAL_RELINK_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libicuuc.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
+    $(TARGET_OUT)/lib64/android.hidl.base@1.0.so
+
 # TWRP Configuration
 RECOVERY_SDCARD_ON_DATA := true
 TARGET_RECOVERY_QCOM_RTC_FIX := true
